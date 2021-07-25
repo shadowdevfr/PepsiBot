8ffa075a0d49ac2bad69e27d3639f0e2183b5eb8
shadowdevfr <pixiwebdev@gmail.com>
2021-07-25 21:56:01 +0200

    hello

--- command_level.vb
+++ command_level.vb
@@ -4,17 +4,19 @@ Module command_level
         Dim eb As EmbedBuilder = New EmbedBuilder
         Dim description As String = "__**Global XP**__" & vbCrLf
         Dim curlvl = twdb.getValue(message.Author.Id & "_XP")
-        If curlvl = "NOT_FOUND" Then
-            curlvl = 0
+        If curlvl.Contains("NOT_FOUND") Then
+            curlvl = New Integer
+            curlvl = 0.15
         End If
         description = description & "> You have :cat2: " & Math.Round(CDec(curlvl), 2) & " cattos around all servers." & vbCrLf & vbCrLf
         Dim guild As IGuildChannel = message.Channel
         description = description & "__**Local XP**__" & vbCrLf
-        curlvl = twdb.getValue(message.Author.Id & "_" & guild.Id & "_XP")
-        If curlvl = "NOT_FOUND" Then
-            curlvl = 0
+        Dim lcurlvl = twdb.getValue(message.Author.Id & "_" & guild.Id & "_XP")
+        If lcurlvl.Contains("NOT_FOUND") Then
+            lcurlvl = New Integer
+            lcurlvl = 0.15
         End If
-        description = description & "> You have :cat2: " & Math.Round(CDec(curlvl), 2) & " cattos on this server." & vbCrLf
+        description = description & "> You have :cat2: " & Math.Round(CDec(lcurlvl), 2) & " cattos on this server." & vbCrLf
         eb.WithTitle("Progress")
         eb.WithDescription(description)
         eb.WithColor(Color.Gold)
--- levelingChat.vb
+++ levelingChat.vb
@@ -3,17 +3,31 @@ Module levelingChat
     Public Sub handleLeveling(message As IMessage)
         ' store GLOBAL xp
         Dim curlvl = twdb.getValue(message.Author.Id & "_XP")
-        If curlvl = "NOT_FOUND" Then
+        If curlvl.Contains("NOT_FOUND") Then
             curlvl = 0
         End If
         twdb.storeValue(message.Author.Id & "_XP", curlvl + config.pointsMultiplier)
 
-        ' store GLOBAL xp
+        ' store GUILD xp
         Dim guild As IGuildChannel = message.Channel
         curlvl = twdb.getValue(message.Author.Id & "_" & guild.Id & "_XP")
-        If curlvl = "NOT_FOUND" Then
+        If curlvl.Contains("NOT_FOUND") Then
             curlvl = 0
         End If
         twdb.storeValue(message.Author.Id & "_" & guild.Id & "_XP", curlvl + config.pointsMultiplier)
+
+        ' store GLOBAL GUILD xp
+        curlvl = twdb.getValue(message.Author.Id & "_" & guild.Id & "_XPGLOBAL")
+        If curlvl.Contains("NOT_FOUND") Then
+            curlvl = 0
+        End If
+        twdb.storeValue(message.Author.Id & "_" & guild.Id & "_XPGLOBAL", curlvl + config.pointsMultiplier)
+
+        ' store GLOBAL xp
+        curlvl = twdb.getValue(message.Author.Id & "_XPGLOBAL")
+        If curlvl.Contains("NOT_FOUND") Then
+            curlvl = 0
+        End If
+        twdb.storeValue(message.Author.Id & "_XPGLOBAL", curlvl + config.pointsMultiplier)
     End Sub
 End Module
