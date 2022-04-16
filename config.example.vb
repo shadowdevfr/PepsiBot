ac7cea4d229d196c56bcce412916b6986586f703
X_Shadow_ <33694505+shadowdevfr@users.noreply.github.com>
2022-04-16 16:23:56 +0200

    Added config!

---
+++ config.example.vb
@@ -0,0 +1,9 @@
+﻿Module config
+    Public botToken As String = ""
+    '
+    ' db config 
+    '
+    Public redisIP As String = ""
+    Public redisPass As String = ""
+    Public pointsMultiplier As Double = 0.75
+End Module
